0f735632bd90d93ff56adfbba7e76d6962617ffa
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2022-06-12 23:43:02 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,11 @@
+import java.net.SocketTimeoutException;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Operators {
+    // arithmetic operators
+    public static void main(String[] args){
+    int num1 = 45, num2 = 45;
+    System.out.println(num1+num2);
+    }
+}
